9d804845549aa8f80aa93b4ccf07b7741796e5ee
nyamadori <nyamadorig@gmail.com>
2017-04-28 23:56:38 +0900

    Remove sample spec

--- spec/juknife_spec.rb
+++ spec/juknife_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Juknife do
   it 'has a version number' do
     expect(Juknife::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
