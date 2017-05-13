85c4c14bea90d91357ac51e15f148ca44ace800e
nyamadori <nyamadorig@gmail.com>
2017-05-13 22:02:13 +0900

    Add simplecov configure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
+require 'simplecov'
+require 'codeclimate-test-reporter'
 require 'juknife'
+
+dir = File.join(ENV['CIRCLE_ARTIFACTS'] || 'build', 'coverage')
+SimpleCov.coverage_dir(dir)
+
+SimpleCov.start do
+  add_filter '/vendor/'
+  add_filter '/spec/'
+
+  formatter SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+  ]
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
